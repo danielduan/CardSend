fbdfe20cbd65deb8b3650dbd46b57d46e9ef1883
Daniel Duan <danielduan88@yahoo.com>
2014-01-17 22:24:42 -0500

    stuffs

---
+++ models/Todo.js
@@ -0,0 +1,15 @@
+var Mongoose = require('mongoose');
+
+exports.TodoSchema = new Mongoose.Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  due: {
+    type: Date,
+    required: true
+  },
+  done: {
+    type: Boolean, default: false
+  }
+});
