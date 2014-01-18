d0a094146c3ed1f432cf18d7c802efb6e9ade0a2
Daniel Duan <danielduan88@yahoo.com>
2014-01-18 05:07:44 -0500

    test send postcard

---
+++ controllers/LobController.js
@@ -0,0 +1,9 @@
+var LOB = new (require('lob'))(process.env.LOBKEY_TEST);
+
+exports.sendPostCard = function(postcard, design) {
+  var fronturl = "@/public/cards/front.pdf";
+  postcard.front = fronturl;
+  LOB.postcards.create(postcard, function(err, res) {
+    console.log(err,res);
+  });
+}
--- routes/apiv1.js
+++ routes/apiv1.js
@@ -1,7 +1,53 @@
 var apicontroller = require("../controllers/APIController");
+var lobcontroller = require("../controllers/LobController");
 
+// gets remaining postcards
+// /api/v1/card/balance?key=_APIKEY_
 exports.cardbalance = function(req, res) {
   var key = req.params.key;
   console.log('Retrieving key: ' + key);
-  res.send(apicontroller.getCardBalance(key));
+  var response = {
+    "key": key,
+    "remaining postcards": apicontroller.getCardBalance(key)
+  };
+  response = JSON.stringify(response);
+  res.send(response);
+}
+
+// charges for additional postcards
+// /api/v1/card/charge?key=_APIKEY
+exports.chargecardbalance = function(req, res) {
+
+}
+
+// send postcards
+// /api/v1/card/send?key=_APIKEY
+to_name, to_address1, to_address2, to_city, to_state, to_zip, to_country,
+from_name, from_address1, from_address2, from_city, from_state,
+from_zip, from_country,
+card_design,
+exports.sendcard = function(req, res) {
+  var addresses = req.body;
+  var cardinfo = {
+    to: {
+      name: to_name,
+      address_line1: to_address1,
+      address_line2: to_address2,
+      address_city: to_city,
+      address_state: to_state,
+      address_zip: to_zip,
+      address_country: to_country
+    },
+    from: {
+      name: from_name,
+      address_line1: from_address1,
+      address_line2: from_address2,
+      address_city: from_city,
+      address_state: from_state,
+      address_zip: from_zip,
+      address_country: from_country
+    },
+    design: card_design
+  };
+  lobcontroller.sendPostCard(cardinfo);
 }
