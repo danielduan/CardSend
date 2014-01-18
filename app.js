ba1264e80974ceb3d85caba853a4fefedd4ba94f
Daniel Duan <danielduan88@yahoo.com>
2014-01-18 13:03:45 -0500

    lets see if static html works

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  //, user = require('./routes/user')
   , apiv1 = require('./routes/apiv1.js')
   , http = require('http')
   , path = require('path');
@@ -12,20 +12,20 @@ var mongoUri = process.env.MONGOHQ_URL ||
   'mongodb://localhost/mydb';
 var db = Mongoose.createConnection(mongoUri);
 
-var TodoSchema = require('./models/Todo.js').TodoSchema;
-var Todo = db.model('todos', TodoSchema);
+//var TodoSchema = require('./models/Todo.js').TodoSchema;
+//var Todo = db.model('todos', TodoSchema);
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -39,11 +39,11 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index(Todo));
-app.get('/users', user.list);
-app.get('/todos.json', routes.get(Todo));
-app.put('/todo/:id.json', routes.update(Todo));
-app.post('/todo.json', routes.addTodo(Todo));
+// app.get('/', routes.index(Todo));
+// app.get('/users', user.list);
+// app.get('/todos.json', routes.get(Todo));
+// app.put('/todo/:id.json', routes.update(Todo));
+// app.post('/todo.json', routes.addTodo(Todo));
 
 //card send v1
 app.get('/api/v1/card/balance', apiv1.cardbalance);
