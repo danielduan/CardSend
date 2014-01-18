ab9ece19b0514599632c60013d96e23f3025c76e
Daniel Duan <danielduan88@yahoo.com>
2014-01-18 05:22:43 -0500

    card url

--- controllers/LobController.js
+++ controllers/LobController.js
@@ -1,7 +1,7 @@
 var LOB = new (require('lob'))(process.env.LOBKEY_TEST);
 
 exports.sendPostCard = function(postcard, design) {
-  var fronturl = "@/public/cards/front.pdf";
+  var fronturl = "@public/cards/front.pdf";
   postcard.front = fronturl;
   LOB.postcards.create(postcard, function(err, res) {
     console.log(err,res);
