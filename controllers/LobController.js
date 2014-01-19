923ccf58fea6f04b9e56da328a173cf90af97c64
Daniel Duan <danielduan88@yahoo.com>
2014-01-19 08:46:46 -0500

    name change

--- controllers/LobController.js
+++ controllers/LobController.js
@@ -2,7 +2,7 @@
 var LOB = new (require('lob'))("test_151c1b8b627229d303c3a7fe5e6c6dc05e7");
 
 exports.sendPostCard = function(postcard, res, design, callback) {
-  var fronturl = "http://postasaservice.herokuapp.com/cards/";
+  var fronturl = "http://ampaas.herokuapp.com/cards/";
   fronturl += design;
   postcard.front = fronturl;
   LOB.postcards.create(postcard, function(err, response) {
