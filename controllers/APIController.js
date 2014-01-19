c85a7c3ad18145a66b7392b958ffe409024f1b8b
Daniel Duan <danielduan88@yahoo.com>
2014-01-19 02:25:53 -0500

    api to mongo

--- controllers/APIController.js
+++ controllers/APIController.js
@@ -1,32 +1,27 @@
-var Mongoose = require('mongoose');
-var mongoUri = process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mydb';
-var db = Mongoose.createConnection(mongoUri);
-
-//stores api keys
-var APIKeysSchema = require('../models/APIKeys.js').APIKeysSchema;
-var APIKeys = db.model('apikeys', APIKeysSchema);
+var apikeysmodel = require('../models/APIKeysModel.js');
 
 exports.getCardBalance = function(key) {
-  db.collection('apikeys', function(err, api) {
-    api.findOne({'key':key}, 'card_balance', function(err, api) {
-        console.log("balance of " + apikey + ": balance " + api.card_balance);
-        return api.card_balance;
-    });
-  });
+  // db.collection('apikeys', function(err, api) {
+  //   api.findOne({'key':key}, 'card_balance', function(err, api) {
+  //       console.log("balance of " + apikey + ": balance " + api.card_balance);
+  //       return api.card_balance;
+  //   });
+  // });
 }
 
 exports.makeAPIKey = function() {
-  var text = "";
+  var key = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for( var i=0; i < 16; i++ ) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
+  apikeysmodel.addNewKey(key);
   //still need to check if key exists, highly doubt it
-  return text;
+  return key;
 }
 
 exports.addCredits = function(apikey, credits) {
+  apikeysmodel.addCardCredit(apikey, credits);
 //   db.findOne({key: apikey }, function (err, account) {
 //   account.card_balance += credits;
 //   account.save(callback);
