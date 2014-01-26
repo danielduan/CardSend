08ce463c0eb46774fc1837f806efc60d5c3f57f2
Daniel Duan <danielduan88@yahoo.com>
2014-01-25 17:11:42 -0800

    test aws

--- controllers/PDFKitController.js
+++ controllers/PDFKitController.js
@@ -1,8 +1,12 @@
 var PDFDocument = require('pdfkit');
 var fs = require('fs');
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-west-2';
+AWS.config.update({accessKeyId: process.env.AWS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_KEY });
 
 exports.createDocument = function(req, res) {
   
+  //generate file name
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
@@ -10,14 +14,28 @@ exports.createDocument = function(req, res) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
 
+
+
   //var image = "../tmp/" + text + ".png";
-  var image = "../tmp/" + text + ".png";
+  //var image = "../tmp/" + text + ".png";
 
   console.log(image);
-  
-  fs.writeFile(image, new Buffer(req.body.image, "base64"), function (err) {
-    if (err) throw err;
-    console.log('It\'s saved!');
+
+  // fs.writeFile(image, new Buffer(req.body.image, "base64"), function (err) {
+  //   if (err) throw err;
+  //   console.log('It\'s saved!');
+  // });
+
+  var s3bucket = new AWS.S3({params: {Bucket: 'postacard-heroku'}});
+  s3bucket.createBucket(function() {
+    var data = {Key: text + ".jpg", Body: new Buffer(req.body.image, "base64")};
+    s3bucket.putObject(data, function(err, data) {
+      if (err) {
+        console.log("Error uploading data: ", err);
+      } else {
+        console.log("Successfully uploaded data to myBucket/myKey");
+      }
+    });
   });
 
 
