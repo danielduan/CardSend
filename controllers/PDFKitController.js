3513b958cbc122e6c627b568ab482916d6ef7949
Daniel Duan <danielduan88@yahoo.com>
2014-01-25 19:16:40 -0800

    test aws

--- controllers/PDFKitController.js
+++ controllers/PDFKitController.js
@@ -21,23 +21,30 @@ exports.createDocument = function(req, res) {
     layout: 'landscape'
   });
 
-  var s3bucket = new AWS.S3({params: {Bucket: 'postacard-heroku'}});
-  //doc.image(new Buffer(req.body.image, "base64"), 100, 100).text('Full size', 100, 85);
-  doc.image(new Buffer(req.body.image), 100, 100).text('Full size', 100, 85);
+  var incomingreq = "";
 
-  doc.output(function(string) {
-    s3bucket.createBucket(function() {
-      var data = {Key: text + ".pdf", ACL:"public-read" , Body: string};
-      s3bucket.putObject(data, function(err, data) {
-        if (err) {
-          console.log("Error uploading data: ", err);
-        } else {
-          console.log("Successfully uploaded pdf to myBucket/myKey");
+  var bufs = [];
+  req.on('data', function(d){
+    bufs.push(d);
+  });
+  req.on('end', function(){
+    var buf = Buffer.concat(bufs);
+    var s3bucket = new AWS.S3({params: {Bucket: 'postacard-heroku'}});
+    doc.image(buf.image, "base64"), 100, 100).text('Full size', 100, 85);
+    doc.output(function(string) {
+      s3bucket.createBucket(function() {
+        var data = {Key: text + ".pdf", ACL:"public-read" , Body: string};
+        s3bucket.putObject(data, function(err, data) {
+          if (err) {
+            console.log("Error uploading data: ", err);
+          } else {
+            console.log("Successfully uploaded pdf to myBucket/myKey");
 
-          var pdfurl = "http://s3-us-west-2.amazonaws.com/postacard-heroku/" + text + ".pdf";
-          res.jsonp({pdf:pdfurl});
-        }
+            var pdfurl = "http://s3-us-west-2.amazonaws.com/postacard-heroku/" + text + ".pdf";
+            res.jsonp({pdf:pdfurl});
+          }
+        });
       });
     });
-  });
+  }
 }
